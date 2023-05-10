b8bffced91db1ef438bdce2ec81997ddab8636c8
Matt <mmaguire333@gmail.com>
2023-05-10 17:23:52 -0600

    Implement onclick function that calls CardContainer state functions from props

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,18 @@
-
 function Card(props) {
+    const updateOnClick = () => {
+        if(!props.clicked) {
+            props.updateCardClicked(props.title);
+            props.updateIndices();
+            props.updateScore();
+        } else {
+            props.resetCurrentScore();
+            props.resetCardsClicked();
+            props.updateIndices();
+        }
+    }
+
     return (
-        <div className="card">
+        <div className="Card" onClick={updateOnClick}>
             <img src={props.src} alt={props.alt}/>
             <p>{props.title}</p>
         </div>
