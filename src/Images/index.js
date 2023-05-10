cd3d932f45786972f094a7bee74d643fdb00a731
Matt <mmaguire333@gmail.com>
2023-05-10 17:24:36 -0600

    Add index.js file to Image folder for easier image imports

---
+++ src/Images/index.js
@@ -0,0 +1,51 @@
+import Abra from './Abra.png';
+import Bellsprout from './Bellsprout.png';
+import Bulbasaur from './Bulbasaur.png';
+import Celebi from './Celebi.png';
+import Charmander from './Charmander.png';
+import Diglett from './Diglett.png';
+import Ekans from './Ekans.png';
+import Elekid from './Elekid.png';
+import Geodude from './Geodude.png';
+import Growlithe from './Growlithe.png';
+import Gyarados from './Gyarados.png';
+import Jigglypuff from './Jigglypuff.png';
+import Jolteon from './Jolteon.png';
+import Meowth from './Meowth.png';
+import Mew from './Mew.png';
+import Pidgey from './Pidgey.png';
+import Pikachu from './Pikachu.png';
+import Quagsire from './Quagsire.png';
+import Rattata from './Rattata.png';
+import Sandshrew from './Sandshrew.png';
+import Snorlax from './Snorlax.png';
+import Spearow from './Spearow.png';
+import Squirtle from './Squirtle.png';
+import Vulpix from './Vulpix.png';
+
+export {
+    Abra,
+    Bellsprout,
+    Bulbasaur,
+    Celebi,
+    Charmander,
+    Diglett,
+    Ekans,
+    Elekid,
+    Geodude,
+    Growlithe,
+    Gyarados,
+    Jigglypuff,
+    Jolteon,
+    Meowth,
+    Mew,
+    Pidgey,
+    Pikachu,
+    Quagsire,
+    Rattata,
+    Sandshrew,
+    Snorlax,
+    Spearow,
+    Squirtle,
+    Vulpix
+}
